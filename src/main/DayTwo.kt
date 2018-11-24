0300d324b6ef11d5a015db16d8c43c305e9e470b
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2018-11-24 13:14:02 +0530

    Day 2 part 2 passed

--- src/main/DayTwo.kt
+++ src/main/DayTwo.kt
@@ -7,10 +7,22 @@ class DayTwo(input: List<String>){
         return x.map { it.split("""\s""".toRegex()).map { e -> e.toInt() } }
     }
 
-     fun part1Solution(): Int {
+    fun part1Solution(): Int {
         return intInput
             .map { it.sorted() }
             .map { it.last() - it.first() }
             .sum()
+    }
+
+    fun part2Solution(): Int = intInput.map { getDivisiblePair(it) }.map { it.second/it.first }.sum()
+
+    private fun getDivisiblePair(x: List<Int>): Pair<Int, Int> {
+        x.reversed().forEach {
+            a -> x.forEach first@ {b ->
+                    if (a == b) return@first
+                    if (a < b && b%a == 0) return a to b
+            }
+        }
+        throw IllegalStateException("no evenly division pair found")
     }
 }
--- src/test/DayTwoTest.kt
+++ src/test/DayTwoTest.kt
@@ -4,6 +4,7 @@ import main.DayTwo
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 internal class DayTwoTest{
     @Test
@@ -34,5 +35,44 @@ internal class DayTwoTest{
                     .toList()
             ).part1Solution() > 0
         )
+    }
+
+//    @BeforeEach
+//    fun setUp() {
+//    }
+
+//
+//    @AfterEach
+//    fun tearDown() {
+//    }
+
+
+    @Test
+    internal fun part2SolutionExampleMatch() {
+        assertEquals(9, DayTwo(listOf("5 9 2 8", "9 4 7 3", "3 8 6 5")).part2Solution(), "part 2 failed")
+    }
+
+    @Test
+    internal fun part2SolutionOnInput() {
+        println(
+            DayTwo(
+                File(
+                    "E:\\codes\\java\\AdventOfCode\\src\\test\\day_two_input.txt"
+                )
+                    .readLines()
+                    .toList()
+            ).part2Solution()
+        )
+
+        assert(
+            DayTwo(
+                File(
+                    "E:\\codes\\java\\AdventOfCode\\src\\test\\day_two_input.txt"
+                )
+                    .readLines()
+                    .toList()
+            ).part2Solution() > 0
+        )
+
     }
 }
